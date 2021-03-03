8601da61aed7bb0ccbb8b6d140df9c7f9392d574
praveen-F1 <70522427+praveen-F1@users.noreply.github.com>
2021-03-03 17:58:54 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+function dis(val) {
+	document.getElementById("inp").value += val
+}
+				
+function solve() {
+	var x = document.getElementById("inp").value
+	var y = eval(x)
+	document.getElementById("inp").value = y 
+}
+								
+function clr() {
+	document.getElementById("inp").value = " "
+}
